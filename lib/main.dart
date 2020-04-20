2532e47ffa4f22540e9aa7d525fc9e9a7e26222c
AndreaS992 <andreastrano92@live.it>
2020-04-20 12:33:02 +0200

    Aggiunto http: ^0.12.0+4 alle dependencies. Creato services/network.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
+Widget provacard() {
+  return Card(
+    color: Color(0x71111111),
+    shape: RoundedRectangleBorder(
+      side: BorderSide(width: 0.2),
+      borderRadius: BorderRadius.circular(18),
+    ),
+    elevation: 5,
+    margin: EdgeInsets.all(15),
+    child: Column(
+//      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Text('Prova'),
+      ],
+    ),
+  );
+}
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -25,11 +43,13 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Color(0xFF111111),
         ),
         body: Container(
+          width: double.infinity,
           decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/images/sfondoridotto.png'),
                 fit: BoxFit.fill),
           ),
+          child: provacard(),
         ));
   }
 }
